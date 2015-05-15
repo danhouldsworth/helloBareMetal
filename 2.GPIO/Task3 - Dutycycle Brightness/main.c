370b1c03470ee6a79208fa47bd8f8f99d12f85b8
Dan Houldsworth <dan@bramble.io>
2015-05-15 12:41:57 +0100

    Nano V4 working

--- 2.GPIO/Task3 - Dutycycle Brightness/main.c
+++ 2.GPIO/Task3 - Dutycycle Brightness/main.c
@@ -5,10 +5,8 @@
 
 int main(void) {
 
-    DDRB  |=  (1 << PORTB7);
-    PORTB |=  (1 << PORTB7);
-    DDRD  |=  (1 << PORTD5);
-    PORTD |=  (1 << PORTD5);
+    DDRB  |=  (1 << PORTB5);
+    PORTB |=  (1 << PORTB5);
 
     double duty_ON = 1.0;
     double period = 20.0;
@@ -16,9 +14,9 @@ int main(void) {
 
     for (;;duty_ON += delta){
         for (int i = 0; i<2;i++) {
-            PORTB |=  (1 << PORTB7);
+            PORTB |=  (1 << PORTB5);
             _delay_ms(duty_ON);
-            PORTB &= ~(1 << PORTB7);
+            PORTB &= ~(1 << PORTB5);
             _delay_ms(period - duty_ON);
         }
         if (duty_ON > (period-1.0) || duty_ON < 1.0) delta =- delta;
